8d63426bb5f1fb702b58ac4c8e164fe76a89c0dc
yuefu_zou <coleman@cienet.com.cn>
2011-05-22 11:19:19 +0800

    make a greate progress of 3D rotate with word changing.

--- src/com/coleman/kingword/activity/CoreActivity.java
+++ src/com/coleman/kingword/activity/CoreActivity.java
@@ -5,15 +5,22 @@ import java.util.ArrayList;
 
 import android.app.Activity;
 import android.content.Context;
-import android.database.Cursor;
+import android.graphics.Camera;
+import android.graphics.Matrix;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.Animation;
+import android.view.animation.DecelerateInterpolator;
+import android.view.animation.Transformation;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.BaseAdapter;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -21,7 +28,6 @@ import com.coleman.kingword.R;
 import com.coleman.kingword.dict.DictManager;
 import com.coleman.kingword.dict.stardict.DictData;
 import com.coleman.kingword.provider.KingWord.SubWordsList;
-import com.coleman.kingword.provider.KingWord.WordListItem;
 import com.coleman.kingword.wordlist.SubWordList;
 
 public class CoreActivity extends Activity implements OnItemClickListener {
@@ -37,6 +43,10 @@ public class CoreActivity extends Activity implements OnItemClickListener {
 
     private SubWordList wordlist;
 
+    private LinearLayout container;
+
+    private String nextWord;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -46,23 +56,53 @@ public class CoreActivity extends Activity implements OnItemClickListener {
         initView();
     }
 
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        new ExpensiveTask(ExpensiveTask.LOOKUP).execute();
+    }
+
     private void initView() {
         textView = (TextView) findViewById(R.id.textView1);
         listView = (ListView) findViewById(R.id.listView1);
+        container = (LinearLayout) findViewById(R.id.container);
         adapter = new ParaphraseAdapter(list);
         listView.setAdapter(adapter);
         listView.setOnItemClickListener(this);
         String word = wordlist.getWord();
         textView.setText(word);
         lookupInDict(word);
+        adapter.notifyDataSetChanged();
+    }
+
+    /**
+     * Setup a new 3D rotation on the container view.
+     * 
+     * @param position the item that was clicked to show a picture, or -1 to
+     *            show the list
+     * @param start the start angle at which the rotation must begin
+     * @param end the end angle of the rotation
+     */
+    private void applyRotation(float start, float end) {
+        // Find the center of the container
+        final float centerX = container.getWidth() / 2.0f;
+        final float centerY = container.getHeight() / 2.0f;
+
+        // Create a new 3D rotation with the supplied parameter
+        // The animation listener is used to trigger the next animation
+        final Rotate3dAnimation rotation = new Rotate3dAnimation(start, end, centerX, centerY,
+                310.0f, true);
+        rotation.setDuration(500);
+        rotation.setFillAfter(true);
+        rotation.setInterpolator(new AccelerateInterpolator());
+        rotation.setAnimationListener(new DisplayNextView());
+
+        container.startAnimation(rotation);
     }
 
     private void lookupInDict(String word) {
+        list.clear();
         DictData dd = DictManager.getInstance().viewWord(this, word);
         list.add(dd);
-        if (adapter != null) {
-            adapter.notifyDataSetChanged();
-        }
     }
 
     private Handler handler = new Handler() {
@@ -118,11 +158,159 @@ public class CoreActivity extends Activity implements OnItemClickListener {
 
     }
 
-    @Override
-    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-        list.clear();
-        String word = wordlist.getNext();
-        textView.setText(word);
-        lookupInDict(word);
+    /**
+     * This class listens for the end of the first half of the animation. It
+     * then posts a new action that effectively swaps the views when the
+     * container is rotated 90 degrees and thus invisible.
+     */
+    private final class DisplayNextView implements Animation.AnimationListener {
+
+        private DisplayNextView() {
+        }
+
+        public void onAnimationStart(Animation animation) {
+        }
+
+        public void onAnimationEnd(Animation animation) {
+            textView.setText(nextWord);
+            if (adapter != null) {
+                adapter.notifyDataSetChanged();
+            }
+            container.post(new SwapViews());
+        }
+
+        public void onAnimationRepeat(Animation animation) {
+        }
+    }
+
+    private class ExpensiveTask extends AsyncTask<Void, Void, Bundle> {
+        private byte type;
+
+        private static final byte LOOKUP = 0;
+
+        public ExpensiveTask(byte type) {
+            this.type = type;
+        }
+
+        @Override
+        protected Bundle doInBackground(Void... params) {
+            Bundle bundle = null;
+            switch (type) {
+                case LOOKUP:
+                    nextWord = wordlist.getNext();
+                    lookupInDict(nextWord);
+                    break;
+                default:
+                    break;
+            }
+            return bundle;
+        }
+
+        @Override
+        protected void onPostExecute(Bundle result) {
+            switch (type) {
+                case LOOKUP:
+                    applyRotation(0, 90);
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+    /**
+     * This class is responsible for swapping the views and start the second
+     * half of the animation.
+     */
+    private final class SwapViews implements Runnable {
+
+        public SwapViews() {
+        }
+
+        public void run() {
+            final float centerX = container.getWidth() / 2.0f;
+            final float centerY = container.getHeight() / 2.0f;
+            Rotate3dAnimation rotation;
+
+            rotation = new Rotate3dAnimation(270, 360, centerX, centerY, 310.0f, false);
+
+            rotation.setDuration(500);
+            rotation.setFillAfter(true);
+            rotation.setInterpolator(new DecelerateInterpolator());
+            container.startAnimation(rotation);
+        }
+    }
+
+    private final class Rotate3dAnimation extends Animation {
+        private final float mFromDegrees;
+
+        private final float mToDegrees;
+
+        private final float mCenterX;
+
+        private final float mCenterY;
+
+        private final float mDepthZ;
+
+        private final boolean mReverse;
+
+        private Camera mCamera;
+
+        /**
+         * Creates a new 3D rotation on the Y axis. The rotation is defined by
+         * its start angle and its end angle. Both angles are in degrees. The
+         * rotation is performed around a center point on the 2D space, definied
+         * by a pair of X and Y coordinates, called centerX and centerY. When
+         * the animation starts, a translation on the Z axis (depth) is
+         * performed. The length of the translation can be specified, as well as
+         * whether the translation should be reversed in time.
+         * 
+         * @param fromDegrees the start angle of the 3D rotation
+         * @param toDegrees the end angle of the 3D rotation
+         * @param centerX the X center of the 3D rotation
+         * @param centerY the Y center of the 3D rotation
+         * @param reverse true if the translation should be reversed, false
+         *            otherwise
+         */
+        public Rotate3dAnimation(float fromDegrees, float toDegrees, float centerX, float centerY,
+                float depthZ, boolean reverse) {
+            mFromDegrees = fromDegrees;
+            mToDegrees = toDegrees;
+            mCenterX = centerX;
+            mCenterY = centerY;
+            mDepthZ = depthZ;
+            mReverse = reverse;
+        }
+
+        @Override
+        public void initialize(int width, int height, int parentWidth, int parentHeight) {
+            super.initialize(width, height, parentWidth, parentHeight);
+            mCamera = new Camera();
+        }
+
+        @Override
+        protected void applyTransformation(float interpolatedTime, Transformation t) {
+            final float fromDegrees = mFromDegrees;
+            float degrees = fromDegrees + ((mToDegrees - fromDegrees) * interpolatedTime);
+
+            final float centerX = mCenterX;
+            final float centerY = mCenterY;
+            final Camera camera = mCamera;
+
+            final Matrix matrix = t.getMatrix();
+
+            camera.save();
+            if (mReverse) {
+                camera.translate(0.0f, 0.0f, mDepthZ * interpolatedTime);
+            } else {
+                camera.translate(0.0f, 0.0f, mDepthZ * (1.0f - interpolatedTime));
+            }
+            camera.rotateY(degrees);
+            camera.getMatrix(matrix);
+            camera.restore();
+
+            matrix.preTranslate(-centerX, -centerY);
+            matrix.postTranslate(centerX, centerY);
+        }
     }
 }
