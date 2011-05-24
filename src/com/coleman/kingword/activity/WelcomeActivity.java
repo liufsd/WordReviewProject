5aab459344141bd5c9264315bda0789737b8983f
yuefu_zou <coleman@cienet.com.cn>
2011-05-24 18:10:32 +0800

    add the feature list

--- src/com/coleman/kingword/activity/WelcomeActivity.java
+++ src/com/coleman/kingword/activity/WelcomeActivity.java
@@ -1,7 +1,10 @@
 
 package com.coleman.kingword.activity;
 
-import java.util.ArrayList;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.http.util.ByteArrayBuffer;
 
 import android.app.Activity;
 import android.content.Intent;
@@ -9,31 +12,76 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.TranslateAnimation;
+import android.view.animation.Animation.AnimationListener;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.coleman.kingword.R;
 import com.coleman.kingword.dict.DictManager;
-import com.coleman.kingword.dict.stardict.DictData;
-import com.coleman.kingword.wordlist.WordList.InternalWordList;
-import com.coleman.kingword.wordlist.WordListManager.IProgressNotifier;
-import com.coleman.kingword.wordlist.WordListManager;
 
 public class WelcomeActivity extends Activity {
     private static final String TAG = WelcomeActivity.class.getName();
 
-    private ArrayList<DictData> list = new ArrayList<DictData>();
+    private Button startButton;
+
+    private TextView featureView;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.welcome);
-        Button button = (Button) findViewById(R.id.w_button1);
-        button.setOnClickListener(new OnClickListener() {
+        startButton = (Button) findViewById(R.id.w_button1);
+        featureView = (TextView) findViewById(R.id.feature);
+        featureView.setText(loadFeatureList());
+        // applyAnim();
+        startButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
                 DictManager.getInstance().initLibrary(WelcomeActivity.this);
                 startActivity(new Intent(WelcomeActivity.this, WordListActivity.class));
             }
         });
+    }
+
+    private void applyAnim() {
+        Animation a = AnimationUtils.loadAnimation(this, R.anim.slide_top_out);
+        a.setDuration(30000);
+        a.setRepeatMode(Animation.RESTART);
+        a.setRepeatCount(Animation.INFINITE);
+        featureView.startAnimation(a);
+    }
+
+    private String loadFeatureList() {
+        long time = System.currentTimeMillis();
+        String str = "no feature found!";
+        try {
+            InputStream is = getAssets().open("kingword/featurelist");
+            int v;
+            byte bytes[] = new byte[1024];
+            ByteArrayBuffer baf = new ByteArrayBuffer(1024 * 200);
+            while ((v = is.read(bytes)) != -1) {
+                baf.append(bytes, 0, v);
+            }
+            is.close();
+            is = null;
+            str = new String(baf.toByteArray());
+            baf.clear();
+            // String str[] = new String(baf.toByteArray()).split("\n");
+            // baf.clear();
+            // baf = null;
+            // for (String string : str) {
+            // if (!TextUtils.isEmpty(string)) {
+            // list.add(string);
+            // }
+            // }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        time = System.currentTimeMillis() - time;
+        Log.d(TAG, "load feature list cost time: " + time);
+        return str;
     }
 }
